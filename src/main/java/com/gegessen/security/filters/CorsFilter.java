1d32c29cb6e975dc74bd7f4bbc6ffa0d586a73f4
Jesper Tjäder <valo_genev@abv.bg>
2021-04-16 11:00:59 +0300

    da

--- src/main/java/com/gegessen/security/filters/CorsFilter.java
+++ src/main/java/com/gegessen/security/filters/CorsFilter.java
@@ -21,7 +21,7 @@ public class CorsFilter implements Filter {
 
         // response.setHeader("Access-Control-Allow-Origin", "https://www.newsbox-bg.com");
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token, jwt-token");
         response.setHeader("Access-Control-Max-Age", "3600");
